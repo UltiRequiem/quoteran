5c4b8d82b7838ba52c547fae14b6f9f438e47ade
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-01 15:16:40 -0500

    :art: Update

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from distutils.core import setup
 from ranquote.version import __version__
 
 setup(
-    name="ranquote",
+    name="quoteran",
     version=__version__,
     description="Random quotes on Terminal",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="UltiRequiem",
     author_email="eliaz.bobadilladev@gmail.com",
-    url="https://github.com/UltiRequiem/ranquote",
+    url="https://github.com/UltiRequiem/quoteran",
     license="MIT",
-    scripts=["./bin/ranquote"],
-    packages=["ranquote"],
+    scripts=["./bin/quoteran"],
+    packages=["quoteran"],
     install_requires=["requests==2.25.1"],
 )
